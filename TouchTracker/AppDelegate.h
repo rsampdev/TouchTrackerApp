f8f76047f78a4c4c3939610e9601d32a3e99707d
rsampdev <rsampdev@gmail.com>
2016-09-15 19:34:51 -0400

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Rodney Sampson on 9/15/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchTracker
+//
+//  Created by Rodney Sampson on 9/15/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
